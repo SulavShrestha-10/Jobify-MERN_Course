a32abcb8fec00cd4190687631c400890f857fff8
SulavShrestha-10 <sulavstha2629@gmail.com>
2024-01-31 15:52:34 +0545

    Minor Change.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = Router();
 const apiLimiter = rateLimiter({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 15,
-	message: { msg: "IP rate limit exceeded, retry in 15 minutes." },
+	message: { message: "IP rate limit exceeded, retry in 15 minutes." },
 });
 router.post("/login", apiLimiter, validateLoginInput, login);
 router.post("/register", apiLimiter, validateRegisterInput, register);
